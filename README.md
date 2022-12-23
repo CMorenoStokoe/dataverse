2a056593ecf56504b4d8ffc53bd15cc231de651f
Chris <48444574+CMorenoStokoe@users.noreply.github.com>
2022-12-23 12:57:46 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# appkit
+ Node web development appkit. Prepared with configuration for a React web app (Create React App) with Tailwind, FontAwesome, Animate.css.
---
+++ app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  const content = 'Hello, world!';
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        Header
+      </header>
+      {content}
+    </div>
+  );
+}
+
+export default App;
