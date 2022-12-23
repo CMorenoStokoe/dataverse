6d919194fe4b5e8de2074d449322d18a819d0460
Chris <48444574+CMorenoStokoe@users.noreply.github.com>
2022-12-23 18:53:22 +0000

    staart

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 function App() {
-  const content = 'Hello, world!';
   
   return (
     <div className="App">
-      <header className="App-header">
-        Header
-      </header>
-      {content}
+      <header className="App-header"/>
+      <button className="p-2 m-2 rounded bg-red-200 text-red-600 hover:bg-red-500 hover:text-red-100 animate__animated animate__bounce animate__infinite">
+        Click me!
+      </button>
     </div>
   );
 }
